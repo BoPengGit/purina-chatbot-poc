1278adcb53269f9f2dfceace6f94b31e410d995f
Bo Peng <bopeng.data@gmail.com>
2024-05-13 23:09:24 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
-import React, { useEffect, useState } from 'react';
+// src/App.js
+import React, { useEffect } from 'react';
+import backgroundImage from './petfinder.png'; // Adjust the image path as per your project
 import './App.css';
 
 function App() {
-  console.log("NODE_ENV = ", process.env.NODE_ENV);
-  console.log("Before API_SERVER_URL = ",process.env.API_SERVER_URL);
-  const [isFetching, setFetching] = useState(false);
-  const [user, setUser] = useState({});
-  const API_SERVER_URL = process.env.API_SERVER_URL || "https://helloworld-lkewgry7ca-de.a.run.app"
-  console.log("After API_SERVER_URL = ",API_SERVER_URL);
-  useEffect(()=> {
-    (async ()=>{
-      setFetching(true);
-      const response = await fetch(API_SERVER_URL+"/user");
-      const userData = await response.json();
-      console.log(userData);
-      setUser(userData)
-      setFetching(false);
-    })();
-  },[0]);
+  // Load the Dialogflow Messenger script after the component mounts
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
 
-  if(isFetching){
-    
-    return <div>Data Loading.....</div>
-  }
   return (
-    
-    <div>
-      Hello World updated from Cloud Run
-      <div>Name: {user.name}</div>
-      <div>Age: {user.age}</div>
+    <div className="App">
+      <header
+        className="fullscreen-background"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: 'contain', // Adjust to cover/contain based on your needs
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          height: '100vh',
+          width: '100vw',
+          position: 'absolute'
+        }}
+      >
+
+        {/* Embed Dialogflow Messenger Widget */}
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <df-messenger
+                oauth-client-id="313705835625-774b02kb87mhr5bd6ja8chqlu0supddk.apps.googleusercontent.com"
+                location="us-central1"
+                project-id="nerdery-technology-gen-ai"
+                agent-id="6f68b946-9042-4a05-b26c-98db9074a37d"
+                language-code="en"
+                max-query-length="-1">
+                <df-messenger-chat-bubble chat-title="Purina Chatbot PoC"></df-messenger-chat-bubble>
+              </df-messenger>
+            `,
+          }}
+        />
+
+        {/* Add custom CSS styles */}
+        <style>
+          {`
+            df-messenger {
+              z-index: 999;
+              position: fixed;
+              bottom: 16px;
+              right: 16px;
+            }
+          `}
+        </style>
+      </header>
     </div>
   );
 }

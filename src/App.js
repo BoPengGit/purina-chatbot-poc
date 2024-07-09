e4cc95fcc7a0f5c6ed9d1e65f3b98b6cc86f5c69
Bo Peng <bopeng.data@gmail.com>
2024-07-09 10:44:24 -0500

    Add affiliate login closetab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import AffiliateLogin from './AffiliateLogin';
+import AffiliateLogin_closeTab from './AffiliateLogin_closeTab';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/affiliate-login" element={<AffiliateLogin />}></Route>
+        <Route path="/affiliate-login2" element={<AffiliateLogin_closeTab />}></Route>
+
       </Routes>
     </BrowserRouter>
   )
